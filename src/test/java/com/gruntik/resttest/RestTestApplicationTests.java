5f1220714e5867633ffc37dbf7a8b3fdc3d2cc02
gruntik <gruntov99@mail.ru>
2022-05-27 12:38:26 +0300

    feat: add tests for repository

--- src/test/java/com/gruntik/resttest/RestTestApplicationTests.java
+++ src/test/java/com/gruntik/resttest/RestTestApplicationTests.java
@@ -2,10 +2,15 @@ package com.gruntik.resttest;
 
 import com.gruntik.resttest.entity.Store;
 import com.gruntik.resttest.repository.StoreRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +21,12 @@ class RestTestApplicationTests {
     @Autowired
     StoreRepository storeRepository;
 
+    @Transactional
+    @BeforeEach
+    public void deleteAll() {
+        storeRepository.deleteAll();
+    }
+
     @Test
     void save() {
         storeRepository.save(new Store("igor", 23));
@@ -24,19 +35,35 @@ class RestTestApplicationTests {
 
     @Test
     void existsByName() {
-        storeRepository.deleteAll();
         storeRepository.save(new Store("igor", 23));
 
         Assertions.assertTrue(storeRepository.existsByName("igor"));
     }
 
+    @Test
+    void findAll() {
+        storeRepository.save(new Store("igor", 23));
+        storeRepository.save(new Store("vas", 30));
+
+        Assertions.assertEquals(2, storeRepository.findAll().size());
+    }
+
     @Transactional
     @Test
     void deleteByName() {
-        storeRepository.deleteAll();
         storeRepository.save(new Store("igor", 23));
 
         Assertions.assertEquals(1, storeRepository.deleteByName("igor"));
+    }
+
+    @Transactional
+    @Test
+    void deleteAllTest() {
+        storeRepository.save(new Store("igor", 23));
+        storeRepository.save(new Store("vas", 30));
+        storeRepository.deleteAll();
+
+        Assertions.assertEquals(0, storeRepository.findAll().size());
     }
 
 }
